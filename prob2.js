b677ef5868b9816c49cffbe2a7c4ec0d0faa74fd
a10192 <102199855+a10192@users.noreply.github.com>
2022-03-28 22:52:03 +0900

    Create prob2.js

---
+++ prob2.js
@@ -0,0 +1,10 @@
+{
+    "name": "이규인",
+    "job": "student",
+    "age": 20,
+    "hobby": "sing",
+    "course_table":[
+        {"name": "미적분학", "id":"MATH101-06"},
+        {"name": "프로그래밍", "id":"CSDE-101"}
+    ]
+}
